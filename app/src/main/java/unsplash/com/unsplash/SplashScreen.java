b44d907e10e5a3f79886a0d8349acefe7d4cdb54
amin <aminullahtajmuhammad@gmail.com>
2018-12-04 01:56:36 +0500

    Added Splash Screen and ProgressBar in this screen

--- app/src/main/java/unsplash/com/unsplash/SplashScreen.java
+++ app/src/main/java/unsplash/com/unsplash/SplashScreen.java
@@ -1,13 +1,41 @@
 package unsplash.com.unsplash;
 
+import android.app.Notification;
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import static android.R.color.black;
 
 public class SplashScreen extends AppCompatActivity {
+
+    ImageView imgSplash;
+    ProgressBar mProgressbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_splash_screen);
+
+        mProgressbar = findViewById(R.id.progressSplash);
+        Timer runSplash = new Timer();
+        TimerTask showSplash = new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        };
+        runSplash.schedule(showSplash,6000);
     }
 }
